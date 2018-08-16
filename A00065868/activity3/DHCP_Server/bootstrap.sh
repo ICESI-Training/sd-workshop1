f46c7c6d458170301d3b09d775ba0d00a1d1f80a
Sistemas Distribuidos <ana_fernanda_25@hotmail.com>
2018-08-15 19:29:03 -0500

    Provisioning for dhcp Server

--- A00065868/activity3/DHCP_Server/bootstrap.sh
+++ A00065868/activity3/DHCP_Server/bootstrap.sh
@@ -2,4 +2,6 @@
 
 sudo yum -y install dhcp
 sudo yum -y install wget
+wget https://raw.githubusercontent.com/AnaValderrama25/sd-workshop0/avalderrama/workshop00/A00065868/activity3/DHCP_Server/dhcpd.conf -O /etc/dhcp/dhcpd.conf
+systemctl start dhcpd
 
