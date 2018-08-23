20fdf4131da30e47d05972052e230a04d7d18bda
Jorge Castaño <jorgeeliecercastao@outlook.com>
2018-08-22 19:00:01 -0500

    update commit

--- A00315284/activity2/dhcp_server/scripts/init.sh
+++ A00315284/activity2/dhcp_server/scripts/init.sh
@@ -1,4 +1,4 @@
 yum install dhcp -y
 yum install wget -y
-wget https://raw.githubusercontent.com/MasterKr123/sd-workshop0/jcastano/workshop0/A00315284/activity2/dhcp_server/scripts/dhcpd.conf -O /etc/dhcp/dhcpd.conf
+wget https://raw.githubusercontent.com/MasterKr123/sd-workshop0/jcastano/sd-workshop0/A00315284/activity2/dhcp_server/scripts/dhcp.conf -O /etc/dhcp/dhcpd.conf
 systemctl start dhcpd
